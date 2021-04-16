da0ddbdb70c7c5ba468247c56e7765e9d03a3173
sofia triana <sofia triana>
2021-04-16 11:12:59 -0500

    desafio 5

--- js/main.js
+++ js/main.js
@@ -1,48 +1,77 @@
 // Desafio 05 y 06
 
+let lugares = alert("¿A que lugar prefieres ir?");
+let opcionesLugares = parseInt(prompt ("1: naeroyfjord Noruega, 2: antelope canyon united states "));
+
 class viaje{
     constructor(lugar, precio, fechas){
         this.lugar = lugar.toUpperCase();
         this.precio  = parseFloat(precio);
         this.fechas = fechas.toUpperCase();
-        this.vendido = false;
     }
     sumaIva() {
         this.precio = this.precio * 1.19;
-    }
-    vender() {
-        this.vendido = true;
     }
     informacionCompleta(){
         console.log('Lugar del viaje ' + this.lugar + ', con un costo de ' + this.precio + ' dolares' + 
         '. Fechas del vuelo: ' + this.fechas);
     }
 }
-let viaje1 = new viaje('naeroyfjord Noruega', 120, 'Del 30 abril al 15 mayo del 2021')
-let viaje2 = new viaje('antelope canyon united states', 110, 'Junio 14 al 20 de junio del 2021')
 
-viaje1.sumaIva();
-viaje2.sumaIva();
-viaje1.vender();
-viaje1.informacionCompleta();
-viaje2.informacionCompleta();
+if (opcionesLugares == 1){
+    let viaje1 = new viaje('naeroyfjord Noruega', 120, 'Del 30 abril al 15 mayo del 2021')
+    viaje1.sumaIva();
+    viaje1.informacionCompleta();
 
-
-//ARRAY
-
-let cities = [
-    'Stanvanguer',
-    'Kristiansand',
-    'Phoenix', 
-    'Jacksonville'
-];
-let planeStanvanguer = {
-    destination: 'Trondheim',
-    transportMode: 'Plane',
-    cost: 200,
-    country: 'Noruega',
+    let citiesNoruega = [
+        'Stanvanguer',
+        'Kristiansand',
+        'Bergen', 
+        'Oslo',
+    ];
+    let planeStanvanguer = {
+        destination: 'Trondheim',
+        transportMode: 'Plane',
+        cost: 200,
+        country: 'Noruega',
+    }
+    citiesNoruega.push(planeStanvanguer);
+    
+    let valorVuelo = parseInt(prompt('Ingrese el valor que puede pagar por un viaje a noruega'))
+    alert('Su valor es de ' + valorVuelo)
+    if (valorVuelo <= 150){
+        alert('No tenemos vuelos disponibles')
+    } else{
+        alert('los lugares a los que puede viajar son: ' + citiesNoruega)
+        console.log(citiesNoruega);
+    }
 }
-cities.push(planeStanvanguer);
-console.log(cities);
+else if (opcionesLugares == 2){
+    let viaje2 = new viaje('antelope canyon united states', 110, 'Junio 14 al 20 de junio del 2021')
+    viaje2.sumaIva();
+    viaje2.informacionCompleta();
+
+    let citiesUsa = [
+        'Chicago',
+        'Phoenix',
+        'Jacksonville', 
+        'Carolina del norte',
+    ];
+    citiesUsa.pop();
+    let valorVueloUs = parseInt(prompt('Ingrese el valor que puede pagar por un viaje a Estados Unidos'))
+    alert('Su valor es de ' + valorVueloUs)
+    if (valorVueloUs <= 150){
+        alert('No hay vuelos con ese valor')
+    } else{
+        alert('los lugares a los que puede viajar son: ' + citiesUsa)
+        console.log(citiesUsa);
+    }
+    
+} else {
+    alert('No tenemos mas vuelos disponibles')
+}
+
+
+
 
 
