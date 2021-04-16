0284b853181e69e2c26b7d48a714084a4c4f24ef
sofia triana <sofia triana>
2021-04-16 10:04:22 -0500

    dasafio 5

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
-const sumar = (num1,num2)=>{
-    return parseInt(num1) + parseInt(num2);
-}
-const restar = (num1,num2)=>{
-    return parseInt(num1) - parseInt(num2);
-}
-const dividir = (num1,num2)=>{
-    return parseInt(num1) / parseInt(num2);
-}
-const multiplicar = (num1,num2)=>{
-    return parseInt(num1) * parseInt(num2);
-}
-alert("¿Que operacion quieres realizar?");
-let operacion = prompt (" 1: suma, 2: resta, 3: divicion, 4: multiplicacion");
-if (operacion == 1){
-    let numero1 = prompt("primer numero para sumar");
-    let numero2 = prompt("segundo numero para sumar");
-    resultado = sumar(numero1, numero2);
-    alert("Tu resultado es " + resultado)
-}
-else if (operacion == 2){
-    let numero1 = prompt("primer numero para restar");
-    let numero2 = prompt("segundo numero para restar");
-    resultado = restar(numero1, numero2)
-    alert("Tu resultado es " + resultado)
-}
-else if (operacion == 3){
-    let numero1 = prompt("primer numero para dividir");
-    let numero2 = prompt("segundo numero para dividir");
-    resultado = dividir(numero1, numero2)
-    alert("Tu resultado es " + resultado)
-}
-else if (operacion == 4){
-    let numero1 = prompt("primer numero para multiplicar");
-    let numero2 = prompt("segundo numero para multiplicar");
-    resultado = multiplicar(numero1, numero2)
-    alert("Tu resultado es " + resultado)
-}
-else {
-    alert("no se encuentra la operación")
-}
+// Desafio 05 y 06
 
-const calcArea = radio =>{
-    return 3.14 * radio**2;
+class viaje{
+    constructor(lugar, precio, fechas){
+        this.lugar = lugar.toUpperCase();
+        this.precio  = parseFloat(precio);
+        this.fechas = fechas.toUpperCase();
+        this.vendido = false;
+    }
+    sumaIva() {
+        this.precio = this.precio * 1.19;
+    }
+    vender() {
+        this.vendido = true;
+    }
+    informacionCompleta(){
+        console.log('Lugar del viaje ' + this.lugar + ', con un costo de ' + this.precio + ' dolares' + 
+        '. Fechas del vuelo: ' + this.fechas);
+    }
 }
-const area = calcArea(5);
-console.log('El area es de ', area);
+let viaje1 = new viaje('naeroyfjord Noruega', 120, 'Del 30 abril al 15 mayo del 2021')
+let viaje2 = new viaje('antelope canyon united states', 110, 'Junio 14 al 20 de junio del 2021')
+
+viaje1.sumaIva();
+viaje2.sumaIva();
+viaje1.vender();
+viaje1.informacionCompleta();
+viaje2.informacionCompleta();
+
+
+//ARRAY
+
+let cities = [
+    'Stanvanguer',
+    'Kristiansand',
+    'Phoenix', 
+    'Jacksonville'
+];
+let planeStanvanguer = {
+    destination: 'Trondheim',
+    transportMode: 'Plane',
+    cost: 200,
+    country: 'Noruega',
+}
+cities.push(planeStanvanguer);
+console.log(cities);
+
 
