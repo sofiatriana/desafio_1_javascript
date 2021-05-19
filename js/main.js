dbee1c9570e98f9c7dfba9d3ad69cc7528e6bb0e
sofia triana <sofia triana>
2021-05-19 11:05:54 -0500

    preload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-const boton = document.getElementsByClassName('btn')
+// loaded page
+$(window).on("load", function(){
+    const preload = $('.display-preload')
+    preload.fadeOut('slow'); 
+}) 
+// Boton click y se agranda
+
+// $(document).ready(function() {
+//     $(".context-box").click(function(){
+//         $(".context-box").animate({
+//             width:'500px',
+//             height: '200px'
+//         }, 1000);
+//         $("content-after").show().animate({
+//             opacity: '1'},3000);
+//     });
+// })
